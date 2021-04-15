c10b917dbbc27a6e0b0c6c486574e47697d1ec04
Mike Seplowitz <mseplowitz@bloomberg.net>
2021-04-15 16:24:25 -0400

    Add BASH_COMPLETION_COMPAT_DIR

--- bash-completion.bash
+++ bash-completion.bash
@@ -2,6 +2,9 @@
 
 if brew --prefix >/dev/null 2>&1 ; then
   if [ -f "$(brew --prefix)/share/bash-completion/bash_completion" ] ; then
+    BASH_COMPLETION_COMPAT_DIR="$(brew --prefix)/etc/bash_completion.d"
+    export BASH_COMPLETION_COMPAT_DIR
+
     # shellcheck source=/dev/null
     source "$(brew --prefix)/share/bash-completion/bash_completion"
   else
